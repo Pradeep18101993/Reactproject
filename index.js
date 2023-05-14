d38fef92f77a2ccc1d09837a6e0f9050fa7ebcaa
pradeep <pradeepraina86@gmail.com>
2023-05-14 10:51:59 +0530

    added backend to the front end

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("How to use Node.js with visual studio code");
+const express=require('express');
+const app=express();
+const cors=require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.get('/api',(req,res)=>{
+  res.json({message:"how are you"});
+})
+
+app.listen(5000,()=>{
+  console.log('started port ');
+})
